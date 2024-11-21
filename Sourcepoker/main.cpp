efa2bceeb090e45f3248caffcc1db68af2f550ca
thienphu <24127490@student.hcmus.edu.vn>
2024-11-21 14:35:45 +0700

    merge with master

--- Sourcepoker/main.cpp
+++ Sourcepoker/main.cpp
@@ -2,30 +2,22 @@
 #include <gameModes.h>
 
 void Games() {
-    cout << "0. Default 5 cards" << endl;
-    cout << "1. Texas Hold 'em" << endl;
-    cout << "2. Draw 5 Poker" << endl;
-    cout << "3. Stud Poker" << endl;
-    int choice; cout << "Enter a number to choose what to do: ";  cin >> choice;
+    std::cout << "0. Default 5 cards" << std::endl;
+    std::cout << "1. Texas Hold 'em" << std::endl;
+    std::cout << "2. Draw 5 Poker" << std::endl;
+    std::cout << "3. Stud Poker" << std::endl;
+    int choice; std::cout << "Enter a number to choose what to do: ";  std::cin >> choice;
 
     int numberOfPlayers, numberOfNPCs;
-    cout << "Enter the totabl number of players (Humans & AIs): "; cin >> numberOfPlayers;
-    cout << "Enter the amount of NPC: "; cin >> numberOfNPCs;
-    House house(new Table(numberOfPlayers, numberOfNPCs));
-
+    std::cout << "Enter the totabl number of players (Humans & AIs): "; std::cin >> numberOfPlayers;
+    std::cout << "Enter the amount of NPC: "; std::cin >> numberOfNPCs;
+    House house = new Table();
+    if (choice == 0) house.setTable(new Table(numberOfPlayers, numberOfNPCs));
     if (choice == 3) house.setTable(new studTable(numberOfPlayers, numberOfNPCs));
     if (choice == 2) house.setTable(new drawTable(numberOfPlayers, numberOfNPCs));
     if (choice == 1) house.setTable(new texasTable(numberOfPlayers, numberOfNPCs));
-    house.populateTable();
-
-    // Deal cards to the player's hand and table.
-    house.dealCardtoPlayers();
-
-    // Display the player's hand and the community card.
-    cout << "Community cards:" << endl;
-    house.showCards();
-    cout << "Player hand:" << endl;
-    house.showPlayersHand();
+    
+    house.StartGame();
 }
 
 int main() {
@@ -66,12 +58,12 @@ int main() {
     //end of application
 
  /*   while (true){
-        cout << "Welcome to poker!" << endl;
-        cout << "Please choose what to do" << endl;
-        cout << "1. Play poker" << endl;
-        cout << "2. view Leaderboard" << endl;
-        cout << "3. Exit program" << endl;
-        int choice; cout << "Enter a number to choose what to do: ";  cin >> choice;
+        std::cout << "Welcome to poker!" << std::endl;
+        std::cout << "Please choose what to do" << std::endl;
+        std::cout << "1. Play poker" << std::endl;
+        std::cout << "2. view Leaderboard" << std::endl;
+        std::cout << "3. Exit program" << std::endl;
+        int choice; std::cout << "Enter a number to choose what to do: ";  std::cin >> choice;
 
         if (choice == 1) Games();
         if (choice == 2) leaderboard().display();
